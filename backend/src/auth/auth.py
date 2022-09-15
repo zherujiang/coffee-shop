e256875f1f520b5717bfe6d6e48a41c5aa3a179e
Cheryl Jiang <cheryl.zjiang@gmail.com>
2022-09-14 23:56:15 -0700

    implement method to verify token header and decode token, return the payload

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -76,7 +76,66 @@ def get_token_auth_header():
     !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
 '''
 def verify_decode_jwt(token):
-    raise Exception('Not Implemented')
+    jsonurl = urlopen(f'https://{AUTH0_DOMAIN}/.well-known/jwks.json')
+    jwks = json.loads(jsonurl.read())
+    token_header = jwt.get_unverified_header(token)
+    rsa_key = dict()
+    print(token_header)
+    print(jwks)
+    
+    if 'kid' not in token_header:
+        raise AuthError(
+            {'code': 'invalid_header',
+             'description':'Authorization malformed.'
+            }, 401
+        )
+    
+    for key in jwks['keys']:
+        if key['kid'] == token_header['kid']:
+            rsa_key = {
+                'kty': key['kty'],
+                'kid': key['kid'],
+                'use': key['use'],
+                'n': key['n'],
+                'e': key['e']
+            }
+    
+    if rsa_key:
+        try:
+            payload = jwt.decode(
+                token,
+                rsa_key,
+                algorithms=ALGORITHMS,
+                audience=API_AUDIENCE,
+                issuer='https://' + AUTH0_DOMAIN + '/'
+            )
+            return payload
+        
+        except jwt.ExpiredSignatureError:
+            raise AuthError(
+                {'code': 'token_expired',
+                'description': 'Token expired.'
+                }, 401
+            )
+        except jwt.JWTClaimsError:
+            raise AuthError(
+                {'code': 'invalid_claims',
+                'description': 'Incorrect claims. Please, check the audience and issuer.'
+                }, 401
+            )
+        except Exception:
+            raise AuthError(
+                {'code': 'invalid_header',
+                'description': 'Unable to parse authentication token.'
+                }, 400
+            )
+    
+    # raise exception when rsa_key is None
+    raise AuthError(
+        {'code': 'invalid_header',
+         'description': 'Unable to find the appropriate key.'
+        }, 400
+    )
 
 
 '''
